7313c059a9b37d3d94af6630e56e993260bf7c9a
kwork <kwork2200@gmail.com>
2026-08-07 17:39:20 +0530

    07/07/2026 Movie Web Online - Implemented ad stacking, where multiple ads are displayed in the same position, with one ad shown after another.
    brijesh_dev_latest

--- lib/core/resources/app_constants.dart
+++ lib/core/resources/app_constants.dart
@@ -10,8 +10,9 @@ class AppConstants {
   static const int carouselSliderItemsCount = 4;
   
   static const String smartLinkUrl =
-      'https://bigotcomet.com/hmr4f43865?key=f1f00a35cf8e26a32e7e8cad972db50d';
-  
+      'https://bigotcomet.com/ndtn3xatmz?key=4615b0f3696e6dfd4ecd1209d18af85c';
+      // 'https://bigotcomet.com/hmr4f43865?key=f1f00a35cf8e26a32e7e8cad972db50d';
+
   /// Opens the SmartLink ad in a new tab/window
   static Future<void> openSmartLink() async {
     try {
